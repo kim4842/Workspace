87dcde575aab1418b9fd8e18072ad06cd2443334
kbj <kbj4842@hanmail.net>
2022-05-21 16:13:20 +0900

    저장

--- example1/src/example3/ShiftOperator.java
+++ example1/src/example3/ShiftOperator.java
@@ -3,7 +3,7 @@ package example3;
 public class ShiftOperator {
 
 	public static void main(String[] args) {
-		//산술 시프트
+		//산술 시프트 비트 2진법 무시 실무 사용안함
 		// @ <<
 		System.out.println(3 << 1);
 		System.out.println(-3 << 1);
